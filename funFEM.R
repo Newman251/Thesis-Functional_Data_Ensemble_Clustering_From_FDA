2a0bce2bc5e5abea880266dd1eec3d8c083e7314
Newman251 <newmanlu@tcd.ie>
2023-02-16 15:57:23 +0000

    Update

--- funFEM.R
+++ funFEM.R
@@ -5,7 +5,7 @@ library(mclust)
 library(tidyverse)
 
 # Load data
-argvals <- seq(0, 1, length.out = 150)
+argvals <- seq(0, 1, length.out = 365)
 values <- t(as.matrix(read.csv('~/ProjectDocs/Project_code/simuations/Data/DataStore.csv', header = FALSE))) # nolint
 labels <- unname(as_vector(read.csv('~/ProjectDocs/Project_code/simuations/Data/LabelStore.csv', header = FALSE))) # nolint
 
@@ -17,7 +17,7 @@ print(length(values))
 basis <- create.bspline.basis(rangeval = c(min(argvals), max(argvals)), nbasis = 25, norder = 3) # nolint
 data_fd <- smooth.basis(argvals = argvals, y = values, fdParobj = basis)$fd
 print(data_fd)
-res_clust = funFEM(data_fd,K=5) # nolint # nolint: commas_linter.
+res_clust = funFEM(data_fd,K=3) # nolint # nolint: commas_linter.
 pred_labels <- res_clust$P
 
 print(pred_labels)
--- funHDDC.R
+++ funHDDC.R
@@ -7,7 +7,7 @@ library(mclust)
 library(tidyverse)
 
 # Load data
-argvals <- seq(0, 1, length.out = 140)
+argvals <- seq(0, 1, length.out = 99)
 values <- t(as.matrix(read.csv('~/ProjectDocs/Project_code/simuations/Data/DataStore.csv', header = FALSE))) # nolint
 labels <- unname(as_vector(read.csv('~/ProjectDocs/Project_code/simuations/Data/LabelStore.csv', header = FALSE))) # nolint
 
@@ -17,9 +17,10 @@ print(length(values))
 
 # FunHDDC
 basis <- create.bspline.basis(rangeval = c(min(argvals), max(argvals)), nbasis = 50, norder = 3) # nolint
+
 data_fd <- smooth.basis(argvals = argvals, y = values, fdParobj = basis)$fd
 print(data_fd)
-res_clust <- funHDDC(data_fd, K = 5, threshold = 0.3, model = 'ABQkDk',
+res_clust <- funHDDC(data_fd, K = 2, threshold = 0.3, model = 'ABQkDk',
 itermax = 2000, eps = 1e-3, init = 'kmeans') # nolint
 pred_labels <- res_clust$class
 
@@ -27,6 +28,5 @@ print(pred_labels)
 
 # Write the pred_labels to a file
 write.csv(pred_labels, file = '~/ProjectDocs/Project_code/simuations/Data/PredictedLabelsStore.csv') # nolint
-
 
 
