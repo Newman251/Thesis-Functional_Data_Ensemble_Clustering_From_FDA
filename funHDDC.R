39a51b4f524c857b88d785141d4aca98297d853d
Newman251 <newmanlu@tcd.ie>
2023-02-20 17:07:10 +0000

    KDE Investigation Plots

--- funHDDC.R
+++ funHDDC.R
@@ -7,7 +7,7 @@ library(mclust)
 library(tidyverse)
 
 # Load data
-argvals <- seq(0, 1, length.out = 365)
+argvals <- seq(0, 1, length.out = 241)
 values <- t(as.matrix(read.csv('~/ProjectDocs/Project_code/simuations/Data/DataStore.csv', header = FALSE))) # nolint
 labels <- unname(as_vector(read.csv('~/ProjectDocs/Project_code/simuations/Data/LabelStore.csv', header = FALSE))) # nolint
 
@@ -20,7 +20,7 @@ basis <- create.bspline.basis(rangeval = c(min(argvals), max(argvals)), nbasis =
 
 data_fd <- smooth.basis(argvals = argvals, y = values, fdParobj = basis)$fd
 print(data_fd)
-res_clust <- funHDDC(data_fd, K = 3, threshold = 0.3, model = 'ABQkDk',
+res_clust <- funHDDC(data_fd, K = 2, threshold = 0.3, model = 'ABQkDk',
 itermax = 2000, eps = 1e-3, init = 'kmeans') # nolint
 pred_labels <- res_clust$class
 
