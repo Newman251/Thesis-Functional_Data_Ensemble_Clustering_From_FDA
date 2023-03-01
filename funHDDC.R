2f4b968aa0d042bc5fb287c8effda0872e13daa5
Newman251 <newmanlu@tcd.ie>
2023-03-01 15:54:28 +0000

    R testing improvement

--- funHDDC.R
+++ funHDDC.R
@@ -7,20 +7,19 @@ library(mclust)
 library(tidyverse)
 
 # Load data
-argvals <- seq(0, 1, length.out = 241)
+argvals <- seq(0, 1, length.out = 30)
 values <- t(as.matrix(read.csv('~/ProjectDocs/Project_code/simuations/Data/DataStore.csv', header = FALSE))) # nolint
-labels <- unname(as_vector(read.csv('~/ProjectDocs/Project_code/simuations/Data/LabelStore.csv', header = FALSE))) # nolint
 
 # Print the length of argvals and values
 print(length(argvals))
 print(length(values))
 
 # FunHDDC
-basis <- create.bspline.basis(rangeval = c(min(argvals), max(argvals)), nbasis = 50, norder = 3) # nolint
+basis <- create.bspline.basis(rangeval = c(min(argvals), max(argvals)), nbasis = 25, norder = 3) # nolint
 
 data_fd <- smooth.basis(argvals = argvals, y = values, fdParobj = basis)$fd
 print(data_fd)
-res_clust <- funHDDC(data_fd, K = 2, threshold = 0.3, model = 'ABQkDk',
+res_clust <- funHDDC(data_fd, K = 4, threshold = 0.3, model = 'ABQkDk',
 itermax = 2000, eps = 1e-3, init = 'kmeans') # nolint
 pred_labels <- res_clust$class
 
